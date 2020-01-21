0c1eec988e09d4e2592d0749d8ab366d1ff78aa2
Eric Born <eric.born85@gmail.com>
2020-01-21 15:30:14 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 14:55:56 2020
+
+@author: Eric
+"""
+
+import numpy as np
+import ImageGrab
+import cv2
+
+while(True):
+    printscreen_pil =  ImageGrab.grab()
+    printscreen_numpy =   np.array(printscreen_pil.getdata(),dtype=uint8)\
+    .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3)) 
+    cv2.imshow('window',printscreen_numpy)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
