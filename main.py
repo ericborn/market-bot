b9ee5c3eaa28bb21a636bf69df6524ec5a69e42b
Eric Born <eric.born85@gmail.com>
2020-01-21 16:03:05 -0800

    Update main.py
    added edge detection

--- main.py
+++ main.py
@@ -6,14 +6,29 @@ Created on Tue Jan 21 14:55:56 2020
 """
 
 import numpy as np
-import ImageGrab
+from PIL import ImageGrab
 import cv2
+import time
+from directkeys import PressKey, ESC, Y, ONE, TWO, THREE,FOUR,\
+                       FIVE, SIX, SEVEN, EIGHT, NINE, ZERO
 
-while(True):
-    printscreen_pil =  ImageGrab.grab()
-    printscreen_numpy =   np.array(printscreen_pil.getdata(),dtype=uint8)\
-    .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3)) 
-    cv2.imshow('window',printscreen_numpy)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+def process_img(image):
+    original_image = image
+    # convert to gray
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # edge detection
+    processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
+    return processed_img
+
+def main():
+    last_time = time.time()
+    while True:
+        screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        #print('Frame took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        new_screen = process_img(screen)
+        cv2.imshow('window', new_screen)
+        #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
